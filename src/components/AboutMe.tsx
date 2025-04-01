6c30b5ed9b5c3b7ac11021044533cf03180901d1
Sneax421 <curilov.german@web.de>
2025-04-01 15:31:50 +0200

    second commit

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -107,16 +107,7 @@ const AboutMe = () => {
     return (
         <>
             {(!!hero) &&
-                <div className='fs-2 lh-lg text-justify ms-5'>
-                    <p><span className='display-3'>name:</span> {hero.name}</p>
-                    <p><span className='display-3'>gender:</span> {hero.gender}</p>
-                    <p><span className='display-3'>birth year:</span> {hero.birth_year}</p>
-                    <p><span className='display-3'>height:</span> {hero.height}</p>
-                    <p><span className='display-3'>mass:</span> {hero.mass}</p>
-                    <p><span className='display-3'>hair color:</span> {hero.hair_color}</p>
-                    <p><span className='display-3'>skin color:</span> {hero.skin_color}</p>
-                    <p><span className='display-3'>eye color:</span> {hero.eye_color}</p>
-                </div>
+
             }
         </>
     );
@@ -125,7 +116,16 @@ const AboutMe = () => {
 export default AboutMe;
 
 
-
+<div className='fs-2 lh-lg text-justify ms-5'>
+    <p><span className='display-3'>name:</span> {hero.name}</p>
+    <p><span className='display-3'>gender:</span> {hero.gender}</p>
+    <p><span className='display-3'>birth year:</span> {hero.birth_year}</p>
+    <p><span className='display-3'>height:</span> {hero.height}</p>
+    <p><span className='display-3'>mass:</span> {hero.mass}</p>
+    <p><span className='display-3'>hair color:</span> {hero.hair_color}</p>
+    <p><span className='display-3'>skin color:</span> {hero.skin_color}</p>
+    <p><span className='display-3'>eye color:</span> {hero.eye_color}</p>
+</div>
 
 
 
